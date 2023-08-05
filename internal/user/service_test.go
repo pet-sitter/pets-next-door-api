13d452595da783dcce26df0e3a96f9bfa25cd330
litsynp <nocte55is@gmail.com>
2023-08-05 17:48:07 +0900

    test: add missing user tests

--- internal/user/service_test.go
+++ internal/user/service_test.go
@@ -45,7 +45,109 @@ func TestUserService(t *testing.T) {
 		})
 
 		t.Run("사용자가 이미 존재할 경우 에러를 반환한다", func(t *testing.T) {
+			tearDown := setUp(t)
+			defer tearDown(t)
 
+			service := NewUserService(db)
+
+			user := &models.User{
+				Email:                "test@example.com",
+				Nickname:             "nickname",
+				Fullname:             "fullname",
+				Password:             "password",
+				FirebaseProviderType: "kakao",
+				FirebaseUID:          "uid",
+			}
+
+			_, _ = service.CreateUser(user)
+			_, err := service.CreateUser(user)
+
+			if err == nil {
+				t.Errorf("got %v want %v", err, nil)
+			}
+		})
+	})
+
+	t.Run("FindUserByEmail", func(t *testing.T) {
+		t.Run("사용자를 이메일로 찾는다", func(t *testing.T) {
+			tearDown := setUp(t)
+			defer tearDown(t)
+
+			service := NewUserService(db)
+
+			user := &models.User{
+				Email:                "test@example.com",
+				Nickname:             "nickname",
+				Fullname:             "fullname",
+				Password:             "password",
+				FirebaseProviderType: "kakao",
+				FirebaseUID:          "uid",
+			}
+
+			created, _ := service.CreateUser(user)
+
+			found, err := service.FindUserByEmail(created.Email)
+			if err != nil {
+				t.Errorf("got %v want %v", err, nil)
+			}
+
+			if found.Email != user.Email {
+				t.Errorf("got %v want %v", found.Email, user.Email)
+			}
+		})
+
+		t.Run("사용자가 존재하지 않을 경우 에러를 반환한다", func(t *testing.T) {
+			tearDown := setUp(t)
+			defer tearDown(t)
+
+			service := NewUserService(db)
+
+			_, err := service.FindUserByEmail("non-existent@example.com")
+			if err == nil {
+				t.Errorf("got %v want %v", err, nil)
+			}
+		})
+	})
+
+	t.Run("FindUserByUID", func(t *testing.T) {
+		t.Run("사용자를 UID로 찾는다", func(t *testing.T) {
+			tearDown := setUp(t)
+			defer tearDown(t)
+
+			service := NewUserService(db)
+
+			user := &models.User{
+				Email:                "test@example.com",
+				Nickname:             "nickname",
+				Fullname:             "fullname",
+				Password:             "password",
+				FirebaseProviderType: "kakao",
+				FirebaseUID:          "uid",
+			}
+
+			created, _ := service.CreateUser(user)
+
+			found, err := service.FindUserByUID(created.FirebaseUID)
+
+			if err != nil {
+				t.Errorf("got %v want %v", err, nil)
+			}
+
+			if found.FirebaseUID != user.FirebaseUID {
+				t.Errorf("got %v want %v", found.FirebaseUID, user.FirebaseUID)
+			}
+		})
+
+		t.Run("사용자가 존재하지 않을 경우 에러를 반환한다", func(t *testing.T) {
+			tearDown := setUp(t)
+			defer tearDown(t)
+
+			service := NewUserService(db)
+
+			_, err := service.FindUserByUID("non-existent")
+			if err == nil {
+				t.Errorf("got %v want %v", err, nil)
+			}
 		})
 	})
 
@@ -77,6 +179,42 @@ func TestUserService(t *testing.T) {
 
 			if status.FirebaseProviderType != user.FirebaseProviderType {
 				t.Errorf("got %v want %v", status.FirebaseProviderType, user.FirebaseProviderType)
+			}
+		})
+	})
+
+	t.Run("UpdateUserByUID", func(t *testing.T) {
+		t.Run("사용자를 업데이트한다", func(t *testing.T) {
+			tearDown := setUp(t)
+			defer tearDown(t)
+
+			service := NewUserService(db)
+
+			user := &models.User{
+				Email:                "test@example.com",
+				Nickname:             "nickname",
+				Fullname:             "fullname",
+				Password:             "password",
+				FirebaseProviderType: "kakao",
+				FirebaseUID:          "uid",
+			}
+
+			created, _ := service.CreateUser(user)
+
+			updatedNickname := "updated"
+
+			_, err := service.UpdateUserByUID(created.FirebaseUID, updatedNickname)
+			if err != nil {
+				t.Errorf("got %v want %v", err, nil)
+			}
+
+			found, err := service.FindUserByUID(created.FirebaseUID)
+			if err != nil {
+				t.Errorf("got %v want %v", err, nil)
+			}
+
+			if found.Nickname != updatedNickname {
+				t.Errorf("got %v want %v", found.Nickname, updatedNickname)
 			}
 		})
 	})
