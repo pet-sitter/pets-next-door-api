6396d224d74e889f77cf16a691722d1d883115a1
litsynp <nocte55is@gmail.com>
2023-09-23 02:52:55 +0900

    chore: v0.4.0

--- pkg/docs/docs.go
+++ pkg/docs/docs.go
@@ -731,7 +731,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "0.3.0",
+	Version:          "0.4.0",
 	Host:             "",
 	BasePath:         "/api",
 	Schemes:          []string{},
