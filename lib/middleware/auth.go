f478aec398e54288f048174f7692d7be4d2a2b91
litsynp <nocte55is@gmail.com>
2023-10-08 21:34:54 +0900

    feat: verify and get user

--- lib/middleware/auth.go
+++ lib/middleware/auth.go
@@ -7,7 +7,7 @@ import (
 	"github.com/pet-sitter/pets-next-door-api/internal/domain/auth"
 )
 
-func BuildAuthMiddleware(app *auth.AuthService, authKey auth.ContextKey) func(next http.Handler) http.Handler {
+func BuildAuthMiddleware(app auth.AuthService, authKey auth.ContextKey) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			ctx := context.WithValue(r.Context(), authKey, app)
