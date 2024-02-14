5bb761b4a4ea6a3b0a7fbe93389505dd2d60f5ff
litsynp <nocte55is@gmail.com>
2024-02-14 21:10:23 +0900

    fix: migration order

--- db/migrations/000008_create_posts_table.down.sql
+++ db/migrations/000008_create_posts_table.down.sql
@@ -1,9 +1,9 @@
-DROP TABLE IF EXISTS sos_posts;
-
-DROP TABLE IF EXISTS sos_conditions;
-
-DROP TABLE IF EXISTS sos_posts_conditions;
+DROP TABLE IF EXISTS resource_media;
 
 DROP TABLE IF EXISTS sos_posts_pets;
 
-DROP TABLE IF EXISTS resource_media;
+DROP TABLE IF EXISTS sos_posts_conditions;
+
+DROP TABLE IF EXISTS sos_conditions;
+
+DROP TABLE IF EXISTS sos_posts;
