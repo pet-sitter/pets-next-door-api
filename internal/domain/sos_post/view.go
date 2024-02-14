e2be0855f873d480aab0878041188f35f975cd5b
choiseyeon <choi20014830@gmail.com>
2024-02-14 16:17:43 +0900

    fix: add user info

--- internal/domain/sos_post/view.go
+++ internal/domain/sos_post/view.go
@@ -1,6 +1,7 @@
 package sos_post
 
 import (
+	"github.com/pet-sitter/pets-next-door-api/internal/domain/user"
 	"time"
 
 	pnd "github.com/pet-sitter/pets-next-door-api/api"
@@ -42,22 +43,22 @@ type WriteSosPostView struct {
 }
 
 type FindSosPostView struct {
-	ID           int               `json:"id"`
-	AuthorID     int               `json:"author_id"`
-	Title        string            `json:"title"`
-	Content      string            `json:"content"`
-	Media        []media.MediaView `json:"media"`
-	Conditions   []ConditionView   `json:"conditions"`
-	Pets         []pet.PetView     `json:"pets"`
-	Reward       string            `json:"reward"`
-	DateStartAt  string            `json:"date_start_at"`
-	DateEndAt    string            `json:"date_end_at"`
-	CareType     CareType          `json:"care_type"`
-	CarerGender  CarerGender       `json:"carer_gender"`
-	RewardAmount RewardAmount      `json:"reward_amount"`
-	ThumbnailID  int               `json:"thumbnail_id"`
-	CreatedAt    time.Time         `json:"created_at"`
-	UpdatedAt    time.Time         `json:"updated_at"`
+	ID           int                          `json:"id"`
+	Author       *user.UserWithoutPrivateInfo `json:"author"`
+	Title        string                       `json:"title"`
+	Content      string                       `json:"content"`
+	Media        []media.MediaView            `json:"media"`
+	Conditions   []ConditionView              `json:"conditions"`
+	Pets         []pet.PetView                `json:"pets"`
+	Reward       string                       `json:"reward"`
+	DateStartAt  string                       `json:"date_start_at"`
+	DateEndAt    string                       `json:"date_end_at"`
+	CareType     CareType                     `json:"care_type"`
+	CarerGender  CarerGender                  `json:"carer_gender"`
+	RewardAmount RewardAmount                 `json:"reward_amount"`
+	ThumbnailID  int                          `json:"thumbnail_id"`
+	CreatedAt    time.Time                    `json:"created_at"`
+	UpdatedAt    time.Time                    `json:"updated_at"`
 }
 
 type FindSosPostListView struct {
