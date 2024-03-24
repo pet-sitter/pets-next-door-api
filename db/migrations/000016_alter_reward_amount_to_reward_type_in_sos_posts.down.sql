e2cfa0a489bc2c081541308a9a15b96564009431
Seyeon_Choi <choi20014830@gmail.com>
2024-03-24 23:10:53 +0900

    fix: alter reward amount to reward type

---
+++ db/migrations/000016_alter_reward_amount_to_reward_type_in_sos_posts.down.sql
@@ -0,0 +1,4 @@
+ALTER TABLE
+    sos_posts
+    RENAME
+        COLUMN reward_type TO reward_amount;
---
+++ db/migrations/000016_alter_reward_amount_to_reward_type_in_sos_posts.up.sql
@@ -0,0 +1,4 @@
+ALTER TABLE
+    sos_posts
+    RENAME
+        COLUMN reward_amount TO reward_type;
