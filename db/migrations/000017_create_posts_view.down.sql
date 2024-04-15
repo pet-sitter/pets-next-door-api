70bdb9b4a41a7036980891864d62c925feb39c90
choiseyeon <choi20014830@gmail.com>
2024-04-16 00:04:07 +0900

    feat: update migrate naming

--- db/migrations/000017_create_posts_view.down.sql
+++ db/migrations/000017_create_posts_view.down.sql
@@ -1,4 +1,4 @@
 DROP VIEW IF EXISTS v_sos_posts;
-DROP VIEW IF EXISTS v_pets;
-DROP VIEW IF EXISTS v_media;
 DROP VIEW IF EXISTS v_conditions;
+DROP VIEW IF EXISTS v_pets_for_sos_posts;
+DROP VIEW IF EXISTS v_media_for_sos_posts;
