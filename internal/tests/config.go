600d54803ce4c87499c1b925326b13509d46ef52
litsynp <nocte55is@gmail.com>
2024-04-19 22:08:28 +0900

    fix(lint): disable gochecknoinits

--- internal/tests/config.go
+++ internal/tests/config.go
@@ -4,6 +4,7 @@ import "os"
 
 var TestDatabaseURL = os.Getenv("TEST_DATABASE_URL")
 
+//nolint:gochecknoinits
 func init() {
 	if TestDatabaseURL == "" {
 		TestDatabaseURL = "postgresql://postgres:postgres@localhost:5455/pets_next_door_api_test?sslmode=disable"
