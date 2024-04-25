245735cd20ff3c9b2161f28fb6400a0a002ea43f
litsynp <nocte55is@gmail.com>
2024-04-26 00:07:29 +0900

    fix: breed.BreedListView swagger reference

--- cmd/server/handler/breed_handler.go
+++ cmd/server/handler/breed_handler.go
@@ -25,7 +25,7 @@ func NewBreedHandler(breedService service.BreedService) *BreedHandler {
 // @Param page query int false "페이지 번호" default(1)
 // @Param size query int false "페이지 사이즈" default(20)
 // @Param pet_type query string false "펫 종류" Enums(dog, cat)
-// @Success 200 {object} pet.BreedListView
+// @Success 200 {object} breed.BreedListView
 // @Router /breeds [get]
 func (h *BreedHandler) FindBreeds(c echo.Context) error {
 	petType := pnd.ParseOptionalStringQuery(c, "pet_type")
