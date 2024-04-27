cf2932b44b0519a1bfd8bd90e1697e96c6b91691
litsynp <nocte55is@gmail.com>
2024-04-27 11:03:14 +0900

    refactor: move to helpers

--- internal/tests/helpers.go
+++ internal/tests/helpers.go
@@ -1,5 +1,24 @@
 package tests
 
+import (
+	"testing"
+
+	"github.com/pet-sitter/pets-next-door-api/internal/infra/database"
+)
+
+func SetUp(t *testing.T) (*database.DB, func(t *testing.T)) {
+	t.Helper()
+
+	db, _ := database.Open(TestDatabaseURL)
+	db.Flush()
+
+	return db, func(t *testing.T) {
+		t.Helper()
+
+		db.Close()
+	}
+}
+
 func CreateForEach(setUp, tearDown func()) func(func()) {
 	return func(test func()) {
 		setUp()
