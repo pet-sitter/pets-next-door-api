2b9e8d8bd7b81f884ce766dee571a1d35bc69751
Seyeon_Choi <choi20014830@gmail.com>
2024-06-03 22:47:31 +0900

    feat: synchronization of chat info when the server is down

--- queries/chats.sql
+++ queries/chats.sql
@@ -59,3 +59,33 @@ WHERE
     room_id = sqlc.narg('room_id')
 ORDER BY created_at DESC
 LIMIT $1 OFFSET $2;
+
+-- name: FindUserChatRooms :many
+SELECT
+    user_chat_rooms.id,
+    user_chat_rooms.user_id,
+    user_chat_rooms.room_id,
+    user_chat_rooms.joined_at,
+    users.email,
+    users.nickname,
+    users.fullname,
+    media.url AS profile_image_url,
+    users.fb_provider_type,
+    users.fb_uid,
+    users.created_at,
+    users.updated_at,
+    chat_rooms.id AS chat_room_id,
+    chat_rooms.name AS chat_room_name,
+    chat_rooms.room_type AS chat_room_type,
+    chat_rooms.created_at AS chat_room_created_at,
+    chat_rooms.updated_at AS chat_room_updated_at
+FROM
+    user_chat_rooms
+    JOIN users
+        ON users.id = user_chat_rooms.user_id
+    JOIN chat_rooms
+        ON chat_rooms.id = user_chat_rooms.room_id
+    LEFT OUTER JOIN media
+        ON users.profile_image_id = media.id
+WHERE
+    user_chat_rooms.left_at IS NULL;
