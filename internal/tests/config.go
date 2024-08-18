4feca45fd55437806d210b67057e947edf988ad0
litsynp <nocte55is@gmail.com>
2024-08-18 10:58:09 +0900

    fix: gochecknoinits using init as name

--- internal/tests/config.go
+++ internal/tests/config.go
@@ -4,7 +4,7 @@ import "os"
 
 var TestDatabaseURL = os.Getenv("TEST_DATABASE_URL")
 
-//nolint:gochecknoinits
+//nolint:init // initializes required environment variables
 func init() {
 	if TestDatabaseURL == "" {
 		TestDatabaseURL = "postgresql://postgres:postgres@localhost:5455/pets_next_door_api_test?sslmode=disable"
