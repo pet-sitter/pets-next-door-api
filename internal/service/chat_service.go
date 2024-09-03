5e181f42faa6c470af3baa99ce4c8ee07c59de38
kimueibin <at365@naver.com>
2024-09-03 15:03:59 +0900

    feat : chat room management api

--- internal/service/chat_service.go
+++ internal/service/chat_service.go
@@ -2,13 +2,12 @@ package service
 
 import (
 	"context"
-
 	pnd "github.com/pet-sitter/pets-next-door-api/api"
 	utils "github.com/pet-sitter/pets-next-door-api/internal/common"
-	databasegen "github.com/pet-sitter/pets-next-door-api/internal/infra/database/gen"
-
 	"github.com/pet-sitter/pets-next-door-api/internal/domain/chat"
 	"github.com/pet-sitter/pets-next-door-api/internal/infra/database"
+	databasegen "github.com/pet-sitter/pets-next-door-api/internal/infra/database/gen"
+	"time"
 )
 
 type ChatService struct {
@@ -105,8 +104,8 @@ func (s *ChatService) SaveMessage(
 	return chat.ToMessage(row), nil
 }
 
-func (s *ChatService) FindRoomByID(ctx context.Context, roomID *int64) (*chat.Room, *pnd.AppError) {
-	row, err := databasegen.New(s.conn).FindRoomByID(ctx, utils.Int64PtrToNullInt32(roomID))
+func (s *ChatService) FindRoomByID(ctx context.Context, roomID int64) (*chat.Room, *pnd.AppError) {
+	row, err := databasegen.New(s.conn).FindRoomByID(ctx, utils.Int64ToNullInt32(roomID))
 	if err != nil {
 		return nil, pnd.FromPostgresError(err)
 	}
@@ -136,4 +135,52 @@ func (s *ChatService) ExistsUserInRoom(ctx context.Context, roomID int64, fbUID 
 		return false, pnd.FromPostgresError(err)
 	}
 	return exists, nil
+}
+
+func (s *ChatService) MockFindAllChatRooms(ctx context.Context) (*[]chat.Room, *pnd.AppError) {
+
+	rooms := []chat.Room{
+		{
+			ID:        1,
+			Name:      "Room 1",
+			RoomType:  chat.RoomTypePersonal,
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+			DeletedAt: time.Now(),
+		},
+		{
+			ID:        2,
+			Name:      "Room 2",
+			RoomType:  chat.RoomTypeGathering,
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+			DeletedAt: time.Now(),
+		},
+	}
+
+	return &rooms, nil
+}
+
+func (s *ChatService) MockFindMessagesByRoomID(ctx context.Context, roomID int64) (*[]chat.Message, *pnd.AppError) {
+	messages := []chat.Message{
+		{
+			ID:          1,
+			RoomID:      roomID,
+			UserID:      1,
+			Content:     "Hello",
+			MessageType: "normal",
+			CreatedAt:   time.Now(),
+			UpdatedAt:   time.Now(),
+		},
+		{
+			ID:          2,
+			RoomID:      roomID,
+			UserID:      2,
+			Content:     "Hi",
+			MessageType: "normal",
+			CreatedAt:   time.Now(),
+			UpdatedAt:   time.Now(),
+		},
+	}
+	return &messages, nil
 }
