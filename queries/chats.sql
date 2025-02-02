0d2a7c75beec010e5a5cdc92e8b9abf0788a2f43
kimueibin <at365@naver.com>
2025-02-02 19:47:45 +0900

    refactor : chat room sql file refactor and sql

--- queries/chats.sql
+++ queries/chats.sql
@@ -2,10 +2,11 @@
 INSERT INTO chat_rooms
 (id,
  name,
+ host_id,
  room_type,
  created_at,
  updated_at)
-VALUES ($1, $2, $3, NOW(), NOW())
+VALUES ($1, $2, $3, $4, NOW(), NOW())
 RETURNING id, name, room_type, created_at, updated_at;
 
 -- name: DeleteRoom :exec
@@ -20,49 +21,62 @@ SELECT EXISTS (SELECT 1
                WHERE room_id = $1
                  AND user_id = $2);
 
--- name: FindRoomByIDAndUserID :one
-SELECT id,
-       name,
-       room_type,
-       created_at,
-       updated_at
-FROM chat_rooms
-WHERE chat_rooms.deleted_at IS NULL
-  AND (chat_rooms.id = $1)
-  AND EXISTS (SELECT 1
-              FROM user_chat_rooms
-              WHERE user_id = $2
-                AND room_id = chat_rooms.id
-                AND left_at IS NULL);
+-- name: FindUserChatRoomByIDAndUserID :one
+SELECT users.id as host_user_id,
+       users.email as host_user_email,
+       users.nickname as host_user_nickname,
+       media.url as host_user_profile_image_url,
+       chat_rooms.id as chat_room_id,
+       chat_rooms.host_id as chat_room_host_id,
+       chat_rooms.name as chat_room_name,
+       chat_rooms.room_type as chat_room_type,
+       chat_rooms.created_at as chat_room_created_at,
+       chat_rooms.updated_at as chat_room_updated_at
+FROM user_chat_rooms
+         JOIN users ON users.id = user_chat_rooms.user_id
+         LEFT OUTER JOIN media ON users.profile_image_id = media.id
+         JOIN chat_rooms ON chat_rooms.id = user_chat_rooms.room_id
+WHERE user_chat_rooms.left_at IS NULL
+    AND chat_rooms.deleted_at IS NULL
+    AND user_chat_rooms.room_id = $1
+    AND user_chat_rooms.user_id = $2;
 
 -- name: FindAllUserChatRoomsByUserUID :many
-SELECT user_chat_rooms.id,
-       user_chat_rooms.user_id,
-       user_chat_rooms.room_id,
-       user_chat_rooms.joined_at,
-       users.email,
-       users.nickname,
-       users.fullname,
-       media.url             AS profile_image_url,
-       users.fb_provider_type,
-       users.fb_uid,
-       users.created_at,
-       users.updated_at,
-       chat_rooms.id         AS chat_room_id,
-       chat_rooms.name       AS chat_room_name,
-       chat_rooms.room_type  AS chat_room_type,
-       chat_rooms.created_at AS chat_room_created_at,
-       chat_rooms.updated_at AS chat_room_updated_at
+SELECT users.id as host_user_id,
+       users.email as host_user_email,
+       users.nickname as host_user_nickname,
+       media.url as host_user_profile_image_url,
+       chat_rooms.id as chat_room_id,
+       chat_rooms.host_id as chat_room_host_id,
+       chat_rooms.name as chat_room_name,
+       chat_rooms.room_type as chat_room_type,
+       chat_rooms.created_at as chat_room_created_at,
+       chat_rooms.updated_at as chat_room_updated_at
 FROM user_chat_rooms
-         JOIN users
-              ON users.id = user_chat_rooms.user_id
-         JOIN chat_rooms
-              ON chat_rooms.id = user_chat_rooms.room_id
-         LEFT OUTER JOIN media
-                         ON users.profile_image_id = media.id
-WHERE user_chat_rooms.left_at IS NULL
-  AND chat_rooms.deleted_at IS NULL
-  AND user_chat_rooms.user_id = $1;
+JOIN users ON users.id = user_chat_rooms.user_id
+LEFT OUTER JOIN media ON users.profile_image_id = media.id
+JOIN chat_rooms ON chat_rooms.id = user_chat_rooms.room_id
+WHERE user_chat_rooms.user_id = $1
+AND user_chat_rooms.left_at IS NULL;
+
+-- name: FindUserInfoByJoinUserId :many
+SELECT users.id            AS join_user_id,
+       users.email        AS join_user_email,
+       users.nickname    AS join_user_nickname,
+       users.fullname   AS join_user_fullname,
+       media.url             AS profile_image_url,
+       users.fb_provider_type AS join_user_fb_provider_type,
+       users.fb_uid      AS join_user_fb_uid,
+       users.created_at  AS join_user_created_at,
+       users.updated_at AS join_user_updated_at
+FROM user_chat_rooms
+JOIN users ON users.id = user_chat_rooms.user_id
+LEFT OUTER JOIN media
+ON users.profile_image_id = media.id
+WHERE user_chat_rooms.room_id = $1
+AND user_chat_rooms.left_at IS NULL
+AND user_chat_rooms.user_id != $2;
+
 
 -- name: JoinRoom :one
 INSERT INTO user_chat_rooms
@@ -101,8 +115,7 @@ SELECT EXISTS (SELECT 1
 SELECT EXISTS (SELECT 1
                FROM chat_rooms
                WHERE id = $1
-                 AND deleted_at IS NULL
-    );
+                 AND deleted_at IS NULL);
 
 
 -- name: FindPrevMessageByRoomID :many
@@ -153,24 +166,20 @@ ORDER BY chat_messages.created_at ASC
 LIMIT $1;
 
 -- name: HasPrevMessages :one
-SELECT EXISTS (
-    SELECT 1
-    FROM chat_messages
-    WHERE chat_messages.deleted_at IS NULL
-      AND room_id = $2
-      AND id < $1  -- 주어진 prev UUID보다 이전 메시지
-    LIMIT 1
-);
+SELECT EXISTS (SELECT 1
+               FROM chat_messages
+               WHERE chat_messages.deleted_at IS NULL
+                 AND room_id = $2
+                 AND id < $1 -- 주어진 prev UUID보다 이전 메시지
+               LIMIT 1);
 
 -- name: HasNextMessages :one
-SELECT EXISTS (
-    SELECT 1
-    FROM chat_messages
-    WHERE chat_messages.deleted_at IS NULL
-      AND room_id = $2
-      AND id > $1  -- 주어진 next UUID보다 이후 메시지
-    LIMIT 1
-);
+SELECT EXISTS (SELECT 1
+               FROM chat_messages
+               WHERE chat_messages.deleted_at IS NULL
+                 AND room_id = $2
+                 AND id > $1 -- 주어진 next UUID보다 이후 메시지
+               LIMIT 1);
 
 -- name: FindMessagesByRoomIDAndSize :many
 SELECT id,
